6ae0b831152cc013a955074106c4152e6176d59b
Fernando Medina Corey <fmcorey@gmail.com>
2019-12-02 15:20:50 -0800

    first change to feature one

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.endpoint = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}. Version 3`,
+      message: `Hello, the current branch is feature-one`,
     }),
   };
 
