9b8cd5d16c3e09d9c674964a8c03d7750bcbdc18
Fernando Medina Corey <fmcorey@gmail.com>
2019-12-05 08:35:28 -0800

    v15

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.endpoint = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}. Version 14`,
+      message: `Hello, the current time is ${new Date().toTimeString()}. Version 15`,
     }),
   };
 
